b38415c0d82393b171553e694653792b15afe705
Blaser993 <corradodepinto.design@gmail.com>
2023-09-11 10:00:28 +0200

    post correzione

--- csharp-oop-shop/Prodotto.cs
+++ csharp-oop-shop/Prodotto.cs
@@ -11,37 +11,42 @@ namespace csharp_oop_shop
     public class Prodotto
     {
         // ATTRIBUTI
-        public int codice {  get; private set; }
-        public string nome { get; set; }
-        public string descrizione { get;  set; }
-        public float prezzo { get; set; }
-        public int iva { get; set; }
+        public int Codice { get; private set; }
+        public string Nome { get; set; }
+        public string Descrizione { get;  set; }
+        public float Prezzo { get; set; }
+        public int Iva { get; set; }
 
         // STATI
         public bool disponibilita;
 
 
         // COSTRUTTORI
-        public Prodotto(int num, string nome, string desc, float prezzo, int iva)
+        public Prodotto( string nome, string desc, float prezzo, int iva)
         {
-            this.codice = num;
-            this.nome = nome;
-            this.descrizione = desc;
-            this.prezzo = prezzo;
-            this.iva = iva;
-  
+            this.Codice = new Random().Next(1, 99999999);
+            this.Nome = nome;
+            this.Descrizione = desc;
+            this.Prezzo = prezzo;
+            this.Iva = iva;
+            this.disponibilita = true;
         }
+
+        //GETTER
+
+        //SETTER
+
 
         //METODI
 
         public void LeggiPrezzo ()
         {
-            Console.WriteLine($"Il prezzo per questo prodotto è {this.prezzo} euro");
+            Console.WriteLine($"Il prezzo per questo prodotto è {this.Prezzo} euro");
         }
 
         public void LeggiPrezzoIvato () 
         {
-            float prezzoIvato = this.prezzo * this.iva / 100;
+            float prezzoIvato = this.Prezzo * this.Iva / 100;
             Console.WriteLine($"Il prezzo più iva di questo prodotto è di {prezzoIvato}");
         }
   
@@ -49,17 +54,14 @@ namespace csharp_oop_shop
         public void LeggiCodiceNome()
         {
             // Formatta il tipo int in string e ne aggiunge gli zeri a seinistra  se necessario.
-            string codiceFormattato = this.codice.ToString("D8");
+            string codiceFormattato = this.Codice.ToString("D8");
 
-            string nomeCompleto = "[" + codiceFormattato +"] "  + this.nome;
+            string nomeCompleto = "[" + codiceFormattato + "]" + this.Nome;
 
-            Console.WriteLine($"Il nome completo del tuo prodotto è {nomeCompleto}");
+            Console.WriteLine($"{nomeCompleto}");
         }
 
-        public void SchedaProdotto()
-        {
 
-        }
 
     }
 }
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -22,13 +22,13 @@ i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in
 
 using csharp_oop_shop;
 
-Prodotto prodotto1 = new Prodotto(34, "Spazzolino elettrico","", 24.5f, 22  );
-Prodotto prodotto2 = new Prodotto(5356, "Dentifricio", "", 0.99f, 22);
-Prodotto prodotto3 = new Prodotto(36604, "Cibo per cani", "", 40f, 22);
-Prodotto prodotto4 = new Prodotto(3004, "Assorbenti", "", 12.9f, 0);
-Prodotto prodotto5 = new Prodotto(11111, "Mazza da golf", "", 124.66f, 22);
+Prodotto prodotto1 = new Prodotto( "Spazzolino elettrico","", 24.5f, 22  );
+Prodotto prodotto2 = new Prodotto( "Dentifricio", "", 0.99f, 22);
+Prodotto prodotto3 = new Prodotto( "Cibo per cani", "", 40f, 22);
+Prodotto prodotto4 = new Prodotto( "Assorbenti", "", 12.9f, 0);
+Prodotto prodotto5 = new Prodotto( "Mazza da golf", "", 124.66f, 22);
 
-Console.WriteLine($"Prodotto1: {prodotto1.nome}");
+Console.WriteLine($"Prodotto1: {prodotto1.Nome}");
 prodotto1.LeggiPrezzo();
 prodotto1.LeggiPrezzoIvato();
 prodotto1.LeggiCodiceNome();
@@ -47,16 +47,17 @@ Prodotto[] negozio = {prodotto1, prodotto2, prodotto3, prodotto4, prodotto5 };
 Console.WriteLine($"All'interno del negozio ci sono i seguenti prodotti: ");
 
 
-for (int i = 0; i < negozio.Length; i++)
+/*for (int i = 0; i < negozio.Length; i++)
 {
-    Console.WriteLine($"Nome del prodotto {i+1} è: {negozio[i].nome}");
+    string nomeCompleto = negozio[i].LeggiCodiceNome();
+    Console.WriteLine($"Nome del prodotto {i+1} è: {negozio[i].Nome}");
 }
 
-/*
+*/
 
 for (int i = 0; i < negozio.Length; i++)
 {
+    Console.Write($"Nome del prodotto {i + 1} è: ");
     negozio[i].LeggiCodiceNome();
 }  
 
-*/
